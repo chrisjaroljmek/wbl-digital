afde2ab2a9f583e1040c84e3e89f642097248b12
Mark Carson <markcarson121@gmail.com>
2023-06-05 14:49:49 +0200

    configured storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,20 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+// .storybook/main.js|ts
+
+import { mergeConfig } from "vite";
+export default {
+  stories: ["../src/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: ["storybook-dark-mode"],
+      },
+    });
+  },
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
-  core: {
-    builder: "@storybook/builder-vite",
-  },
 };
-export default config;
