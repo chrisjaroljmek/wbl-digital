3478a5640e6c77ec0279a3caa9fcb9178e9194dd
Mark Carson <markcarson121@gmail.com>
2023-06-05 15:46:01 +0200

    revert

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,8 @@
-const tailwindConfig = require("./src/tailwind/tailwind.config");
-
-module.exports = tailwindConfig;
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
