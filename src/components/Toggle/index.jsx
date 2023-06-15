0450fd0cc459dd0901d0d285a9602aa3b8674867
Mark Carson <markcarson121@gmail.com>
2023-06-15 16:32:58 +0200

    imported icons into Toggle component

--- src/components/Toggle/index.jsx
+++ src/components/Toggle/index.jsx
@@ -1,3 +1,11 @@
+import sun from "../../assets/icons/sun.svg";
+import moon from "../../assets/icons/moon.svg";
+
 export function Toggle() {
-  return <div>Toggle</div>;
+  return (
+    <div className="flex">
+      <img src={sun} alt="light" />
+      <img src={moon} alt="dark" />
+    </div>
+  );
 }
