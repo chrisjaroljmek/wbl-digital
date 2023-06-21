fbaeafdee21bb981c9c15bf5ec81a78c68477779
Christian Jaroljmek <christian@jaroljmek.com>
2023-06-21 09:21:04 +0100

    Site structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 export function App() {
-  return <div className="text-primary-a">App</div>;
+  return (
+    <div>
+      <Header />
+      <section id="1" className="bg-blue-500 h-screen">
+        <h2>Landing Page</h2>
+      </section>
+      <section id="2" className="bg-green-500 h-screen">
+        <h2>About us</h2>
+      </section>
+      <section id="3" className="bg-red-500 h-screen">
+        <h2>What we do</h2>
+      </section>
+      <section id="4" className="bg-purple-500 h-screen">
+        <h2>Our clients</h2>
+      </section>
+      <section id="5" className="bg-yellow-500 h-screen">
+        <h2>Get in touch</h2>
+      </section>
+    </div>
+  );
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+export function Header() {
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <header className="fixed">
+      <button onClick={() => scrollToSection("2")}>About us</button>
+      <button onClick={() => scrollToSection("3")}>What we do</button>
+      <button onClick={() => scrollToSection("4")}>Our clients</button>
+      <button onClick={() => scrollToSection("5")}>Get in touch</button>
+    </header>
+  );
+}
