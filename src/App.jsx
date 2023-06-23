a1bcf597a7a8b4c1759b2ac5bc74e2d83158a8d6
Mark Carson <markcarson121@gmail.com>
2023-06-23 21:08:52 +0200

    added javascript to detect current section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
-import "./App.css";
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { Logo } from "./components/Logo";
 
+import "./App.css";
+
 export function App() {
+  const [currentSection, setCurrentSection] = useState("");
+
+  useEffect(() => {
+    const onScroll = () => {
+      const sections = document.querySelectorAll("section");
+      let current = "";
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition >= sectionTop - sectionHeight / 5) {
+          current = section.id;
+        }
+      });
+
+      setCurrentSection(current);
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  console.log("currentSection", currentSection);
+
   return (
     <div>
       <Header />
