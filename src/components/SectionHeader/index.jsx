f9f405ee4b9414eefd450fc2d0b00bb62f91b507
Mark Carson <markcarson121@gmail.com>
2023-06-26 19:25:09 +0200

    added props and proptypes to SectionHeader

--- src/components/SectionHeader/index.jsx
+++ src/components/SectionHeader/index.jsx
@@ -1,8 +1,12 @@
+import PropTypes from "prop-types";
 import classnames from "classnames";
 
 export function SectionHeader(props) {
-  const { className } = props;
-  return (
-    <div className={classnames("underline", className)}>SectionHeader</div>
-  );
+  const { text, className } = props;
+  return <div className={classnames(className)}>{text}</div>;
 }
+
+SectionHeader.propTypes = {
+  text: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
