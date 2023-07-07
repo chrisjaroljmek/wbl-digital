5e2c6dce07222ebbb09702b178aeb1863a841efc
Christian Jaroljmek <christian@jaroljmek.com>
2023-07-07 17:08:42 +0100

    creating layout for section and shapes

--- src/components/Shape/index.jsx
+++ src/components/Shape/index.jsx
@@ -6,7 +6,9 @@ export function Shape(props) {
     <div
       className={classnames(
         "border-2",
-        shape === "circle" && "h-12 w-12 ",
+        shape === "circle" && "h-12 w-12 rounded-full",
+        shape === "square" && "h-12 w-12",
+        shape === "diamond" && "h-12 w-12 transform rotate-45",
         className
       )}
     ></div>
