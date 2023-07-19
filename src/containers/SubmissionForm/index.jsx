3b0054b6fd4f1272bce05ee22525e25357c3191b
Mark Carson <markcarson121@gmail.com>
2023-07-19 10:44:47 +0200

    added onChange and onSubmit functions

--- src/containers/SubmissionForm/index.jsx
+++ src/containers/SubmissionForm/index.jsx
@@ -1,15 +1,37 @@
-import {useState} from React;
+import { useState } from "react";
 
 export function SubmissionForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
+  function onSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="flex flex-col items-center justify-center">
-      <input type="text" value={name} placeholder="Name" />
-      <input type="text" value={email} placeholder="Email" />
-      <input type="text" value={message}placeholder="Message" />
+    <form
+      className="flex flex-col items-center justify-center"
+      onSubmit={onSubmit}
+    >
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+      />
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Message"
+      />
       <button type="submit">Send</button>
     </form>
   );
