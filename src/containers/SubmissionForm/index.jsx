9d2d6e3420fb09b05e3b442c99be4b2d745c5c25
Mark Carson <markcarson121@gmail.com>
2023-07-19 10:37:15 +0200

    added JSX for SubmissionForm

--- src/containers/SubmissionForm/index.jsx
+++ src/containers/SubmissionForm/index.jsx
@@ -1,3 +1,10 @@
 export function SubmissionForm() {
-  return <div>Submission Form</div>;
+  return (
+    <form>
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Email" />
+      <input type="text" placeholder="Message" />
+      <button type="submit">Send</button>
+    </form>
+  );
 }
