e21842ddc00572f84631671822b7eef5fad61ee5
Mark Carson <markcarson121@gmail.com>
2023-07-19 10:41:19 +0200

    added state to SubmissionForm

--- src/containers/SubmissionForm/index.jsx
+++ src/containers/SubmissionForm/index.jsx
@@ -1,9 +1,15 @@
+import {useState} from React;
+
 export function SubmissionForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <form className="flex flex-col items-center justify-center">
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Email" />
-      <input type="text" placeholder="Message" />
+      <input type="text" value={name} placeholder="Name" />
+      <input type="text" value={email} placeholder="Email" />
+      <input type="text" value={message}placeholder="Message" />
       <button type="submit">Send</button>
     </form>
   );
