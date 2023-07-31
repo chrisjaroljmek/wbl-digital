6ba641e51d6a7ea260981ef9560c13a3db3b94c4
Mark Carson <markcarson121@gmail.com>
2023-07-31 09:47:24 +0200

    added hover state to Skill component

--- src/components/Skill/index.jsx
+++ src/components/Skill/index.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
+
 import PropTypes from "prop-types";
 import classnames from "classnames";
 
 export function Skill(props) {
   const { title, description, className } = props;
+  const [hover, setHover] = useState(false);
+
+  const onMouseEnter = () => {
+    setHover(true);
+  };
+
+  const onMouseLeave = () => {
+    setHover(false);
+  };
 
   return (
-    <div className={classnames("border-t border-grey w-full", className)}>
+    <div
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      className={classnames("border-t border-grey w-full", className)}
+    >
       <div className="flex my-16 mx-8">
         <div className="mr-8">
           <p className="text-[4vw] leading-none whitespace-nowrap">{title}</p>
-          <div className="h-[2px] mt-0.5 bg-grey"></div>
+          <div
+            className={classnames(
+              "h-[2px] mt-0.5 bg-grey",
+              hover && "h-[20px]"
+            )}
+          ></div>
         </div>
         <p className="text-[2.8vw] text-grey leading-none">{description}</p>
       </div>
