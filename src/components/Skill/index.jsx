dc3d686897eac57a6299e3fd3f3cf9bf76183f2c
Mark Carson <markcarson121@gmail.com>
2023-07-31 09:39:22 +0200

    fixed color naming in Skill

--- src/components/Skill/index.jsx
+++ src/components/Skill/index.jsx
@@ -5,15 +5,13 @@ export function Skill(props) {
   const { title, description, className } = props;
 
   return (
-    <div className={classnames("border-t border-dark-grey w-full", className)}>
+    <div className={classnames("border-t border-grey w-full", className)}>
       <div className="flex my-16 mx-8">
         <div className="mr-8">
           <p className="text-[4vw] leading-none whitespace-nowrap">{title}</p>
-          <div className="h-[2px] mt-0.5 bg-dark-grey"></div>
+          <div className="h-[2px] mt-0.5 bg-grey"></div>
         </div>
-        <p className="text-[2.8vw] text-dark-grey leading-none">
-          {description}
-        </p>
+        <p className="text-[2.8vw] text-grey leading-none">{description}</p>
       </div>
     </div>
   );
