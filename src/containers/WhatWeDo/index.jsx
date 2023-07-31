4e1e06b797d92f95f2138826be99f6ab29b73250
Mark Carson <markcarson121@gmail.com>
2023-07-31 09:40:26 +0200

    removed header for WhatWeDo section

--- src/containers/WhatWeDo/index.jsx
+++ src/containers/WhatWeDo/index.jsx
@@ -3,7 +3,6 @@ import { Skill } from "../../components/Skill";
 export function WhatWeDo() {
   return (
     <div className="w-full px-16">
-      <h1 className="ml-8 mb-4 text-[2vw]">What we do</h1>
       <Skill
         title="We add purpose"
         description="We think of a brand as a promise
