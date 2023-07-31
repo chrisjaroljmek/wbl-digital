a08bd57e6cee9781c6926bff1ce8e6cb63aaf4a3
Mark Carson <markcarson121@gmail.com>
2023-07-31 09:31:13 +0200

    removed title from section props

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -2,11 +2,10 @@ import PropTypes from "prop-types";
 import classnames from "classnames";
 
 export function Section(props) {
-  const { id, title, children, className } = props;
+  const { id, children, className } = props;
 
   return (
     <section id={id} className={classnames(className)}>
-      <h2>{title}</h2>
       {children}
     </section>
   );
@@ -14,7 +13,6 @@ export function Section(props) {
 
 Section.propTypes = {
   id: PropTypes.string.isRequired,
-  title: PropTypes.string,
   children: PropTypes.any,
   className: PropTypes.string,
 };
