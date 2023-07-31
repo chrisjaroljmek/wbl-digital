2eda1fbc0cb45dce97d474576c46b270b0e528f5
Mark Carson <markcarson121@gmail.com>
2023-07-31 10:25:41 +0200

    added hover effect to title

--- src/components/Skill/index.jsx
+++ src/components/Skill/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import classnames from "classnames";
 
 export function Skill(props) {
-  const { title, description, className } = props;
+  const { color, title, description, className } = props;
   const [hover, setHover] = useState(false);
 
   const onMouseEnter = () => {
@@ -16,19 +16,30 @@ export function Skill(props) {
   };
 
   return (
-    <div
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      className={classnames("border-t border-grey w-full", className)}
-    >
-      <div className="flex my-16 mx-8">
+    <div className={classnames("border-t border-grey w-full", className)}>
+      <div
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        className="flex my-16 mx-8"
+      >
         <div className="mr-8">
-          <p className="text-[4vw] leading-none whitespace-nowrap">{title}</p>
+          <p className="relative drop-shadow-xl text-[4vw] leading-none whitespace-nowrap z-50">
+            {title}
+          </p>
           <div
             className={classnames(
-              "h-[2px] mt-0.5 bg-grey",
-              hover && "h-[20px]"
+              "mt-1",
+              hover && color === "blue" && "bg-darkBlue",
+              hover && color === "green" && "bg-green",
+              hover && color === "red" && "bg-red",
+              !hover && "bg-grey"
             )}
+            style={{
+              height: "0.125rem",
+              transition: "transform 0.3s ease",
+              transformOrigin: "bottom",
+              transform: `scaleY(${hover ? 8 : 1})`,
+            }}
           ></div>
         </div>
         <p className="text-[2.8vw] text-grey leading-none">{description}</p>
--- src/containers/WhatWeDo/index.jsx
+++ src/containers/WhatWeDo/index.jsx
@@ -4,24 +4,21 @@ export function WhatWeDo() {
   return (
     <div className="w-full px-16">
       <Skill
+        color="blue"
         title="We add purpose"
         description="We think of a brand as a promise
 and deliver digital brand experiences
 that users can believe in."
       />
       <Skill
+        color="green"
         title="We add purpose"
         description="We think of a brand as a promise
 and deliver digital brand experiences
 that users can believe in."
       />
       <Skill
-        title="We add purpose"
-        description="We think of a brand as a promise
-and deliver digital brand experiences
-that users can believe in."
-      />
-      <Skill
+        color="red"
         title="We add purpose"
         description="We think of a brand as a promise
 and deliver digital brand experiences
