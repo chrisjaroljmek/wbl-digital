656b9c3fe4d67a5e7f1cddbe21bc8dda844a3da8
Mark Carson <markcarson121@gmail.com>
2023-08-21 21:57:20 +0200

    made progress with carousel

--- src/components/ClientCarousel/index.jsx
+++ src/components/ClientCarousel/index.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, useRef } from "react";
+
 import { ClientCard } from "../ClientCard";
 
 import companyOne from "../../assets/images/101.svg";
@@ -17,21 +19,67 @@ const clients = [
 ];
 
 export function ClientCarousel() {
-  const movePrev = () => {};
-  const moveNext = () => {};
+  const maxScrollWidth = useRef(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const carousel = useRef(null);
+
+  const movePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevState) => prevState - 1);
+    }
+  };
+
+  const moveNext = () => {
+    if (
+      carousel.current !== null &&
+      carousel.current.offsetWidth * currentIndex <= maxScrollWidth.current
+    ) {
+      setCurrentIndex((prevState) => prevState + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (carousel !== null && carousel.current !== null) {
+      carousel.current.scrollLeft = carousel.current.offsetWidth * currentIndex;
+    }
+  }, [currentIndex]);
+
+  useEffect(() => {
+    maxScrollWidth.current = carousel.current
+      ? carousel.current.scrollWidth - carousel.current.offsetWidth
+      : 0;
+  }, []);
 
   return (
     <>
       <div className="relative overflow-hidden">
-        {clients.map(({ image, name, type }, index) => (
-          <div key={index}>
-            <ClientCard image={image} name={name} type={type} />
-          </div>
-        ))}
-      </div>
-      <div className="flex justify-between absolute top left w-full h-full">
-        <button onClick={movePrev}>Prev</button>
-        <button onClick={moveNext}>Next</button>
+        <div className="flex justify-between absolute top left w-full h-full">
+          <button
+            className="w-10 h-full z-10 p-0 m-0 transition-all ease-in-out duration-300"
+            onClick={movePrev}
+          >
+            Prev
+          </button>
+          <button
+            className="w-10 h-full z-10 p-0 m-0 transition-all ease-in-out duration-300"
+            onClick={moveNext}
+          >
+            Next
+          </button>
+        </div>
+        <div
+          ref={carousel}
+          className="carousel-container relative flex gap-1 overflow-hidden scroll-smooth snap-x snap-mandatory touch-pan-x z-0"
+        >
+          {clients.map(({ image, name, type }, index) => (
+            <div
+              key={index}
+              className="carousel-item relative w-64 h-64 snap-start"
+            >
+              <ClientCard image={image} name={name} type={type} />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
