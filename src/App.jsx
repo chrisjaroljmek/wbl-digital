3ad296c33ab775581d4a10334805f5c57dd1dcbb
Mark Carson <markcarson121@gmail.com>
2023-09-15 16:39:04 +0100

    WhatWeDo desktop complete

--- src/App.jsx
+++ src/App.jsx
@@ -94,61 +94,61 @@ export function App() {
           </div>
         </div>
       </Section>
-      <Section className="flex flex-col items-center" id="2">
-        <div className="flex relative mb-32">
-          <div className="w-48 h-48 mr-32 display flex items-center justify-center bg-gradient-to-tr from-darkBlue to-blue rounded-full">
+      <Section className="flex flex-col items-center lg:items-start" id="2">
+        <div className="flex relative mb-32 lg:mb-48">
+          <div className="w-48 h-48 lg:w-80 lg:h-80 mr-32 lg:mr-0 lg:ml-32 display flex items-center justify-center bg-gradient-to-tr from-darkBlue to-blue rounded-full">
             <div
               className={classnames(
-                "w-[184px] h-[184px]",
+                "w-[184px] lg:w-[312px] h-[184px] lg:h-[312px]",
                 darkMode === true && "bg-black rounded-full",
                 darkMode === false && "bg-white rounded-full"
               )}
             ></div>
           </div>
-          <div className="absolute top-10 left-16">
-            <p className="font-semibold text-3xl drop-shadow-2xl">
+          <div className="absolute top-10 left-16 lg:top-28 lg:left-64">
+            <p className="font-semibold text-3xl lg:text-5xl drop-shadow-2xl lg:w-[500px]">
               We produce websites and style brands.
             </p>
-            <p className="text-grey text-base hidden lg:block">
+            <p className="text-grey text-3xl mt-2 ml-48 hidden lg:block w-[500px]">
               We think of a brand as a promise. We create digital brand
               experiences for users to believe in.
             </p>
           </div>
         </div>
-        <div className="ml-32 relative mb-32">
-          <div className="w-36 h-36 display flex items-center justify-center bg-gradient-to-tr from-red to-orange rounded-full">
+        <div className="lg:flex lg:justify-end lg:w-full ml-32 lg:ml-0 relative mb-32 lg:mb-48 lg:pr-48">
+          <div className="w-36 h-36 lg:w-64 lg:h-64 display flex items-center justify-center bg-gradient-to-tr from-red to-orange rounded-full">
             <div
               className={classnames(
-                "h-[136px] w-[136px]",
+                "h-[136px] lg:h-[248px] w-[136px] lg:w-[248px]",
                 darkMode === true && "bg-black rounded-full",
                 darkMode === false && "bg-white rounded-full"
               )}
             ></div>
           </div>
-          <div className="absolute top-4 right-12 w-[300px] text-right drop-shadow-2xl">
-            <p className="font-semibold text-3xl">
+          <div className="absolute top-4 right-12 lg:top-20 lg:right-[440px] w-[300px] text-right lg:text-left drop-shadow-2xl">
+            <p className="font-semibold text-3xl lg:text-5xl lg:w-[600px]">
               We are creatives, coders and strategists.
             </p>
-            <p className="text-grey text-base hidden lg:block">
+            <p className="text-grey text-3xl mt-2 ml-48 lg:ml-0 hidden lg:block lg:w-[400px]">
               We cross disciplines and cross borders. We are your team.
             </p>
           </div>
         </div>
-        <div className="flex relative mb-32 mr-32">
-          <div className="w-24 h-24 display flex items-center justify-center bg-gradient-to-tr from-green to-green rounded-full">
+        <div className="flex relative mb-32 lg:mb-48 mr-32 lg:mr-0 lg:ml-32">
+          <div className="w-24 h-24 lg:w-48 lg:h-48 display flex items-center justify-center bg-gradient-to-tr from-green to-green rounded-full">
             <div
               className={classnames(
-                "h-[88px] w-[88px]",
+                "h-[88px] w-[88px] lg:h-[184px] lg:w-[184px]",
                 darkMode === true && "bg-black rounded-full",
                 darkMode === false && "bg-white rounded-full"
               )}
             ></div>
           </div>
-          <div className="absolute top-8 left-12">
-            <p className="font-semibold text-3xl whitespace-nowrap">
+          <div className="absolute top-8 lg:top-16 lg:mt-2 left-12 lg:left-20">
+            <p className="font-semibold text-3xl lg:text-5xl whitespace-nowrap">
               We add purpose.
             </p>
-            <p className="text-grey text-base hidden lg:block">
+            <p className="text-grey text-3xl mt-2 ml-48 lg:ml-32 hidden lg:block w-[500px]">
               We gel strategy with creativity. Our web products display form and
               deliver function.
             </p>
